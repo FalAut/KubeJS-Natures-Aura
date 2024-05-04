57c50532e9b300a761902acda0015b1cfc99276c
FalAut <143469089+FalAut@users.noreply.github.com>
2024-05-04 10:43:58 +0800

    忘切分支了啊啊啊啊啊

--- src/main/java/com/falaut/KubeJSNaturesAura.java
+++ src/main/java/com/falaut/KubeJSNaturesAura.java
@@ -2,16 +2,16 @@ package com.falaut;
 
 import com.falaut.event.NaturesAuraEventJS;
 import dev.latvian.mods.kubejs.script.ScriptType;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartedEvent;
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartedEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(KubeJSNaturesAura.MOD_ID)
 public class KubeJSNaturesAura {
     public static final String MOD_ID = "kubejs_naturesaura";
 
     public KubeJSNaturesAura() {
-        NeoForge.EVENT_BUS.addListener(KubeJSNaturesAura::gameStart);
+        MinecraftForge.EVENT_BUS.addListener(KubeJSNaturesAura::gameStart);
     }
     public static void gameStart(ServerStartedEvent event) {
         KubeJSNaturesAuraPlugin.AURA_TYPE.post(ScriptType.SERVER, NaturesAuraEventJS.INSTANCE);
